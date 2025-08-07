56587b1a83360e21c8499bafb8d4ed85f907cf4c
Phil Chiu <phil.chiu@archer.com>
2025-08-07 17:54:14 -0600

    try build.sh from numpy

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -ex
 
-# binary is called `pkg-config`, but package metadata is under `lib/pkgconfig`
-export MESON_ARGS="${MESON_ARGS} --pkg-config-path=${PREFIX}/lib/pkgconfig"
+# necessary for cross-compilation to point to the right env
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+
+mkdir builddir
+
+# HACK: extend $CONDA_PREFIX/meson_cross_file that's created in
+# https://github.com/conda-forge/ctng-compiler-activation-feedstock/blob/main/recipe/activate-gcc.sh
+# https://github.com/conda-forge/clang-compiler-activation-feedstock/blob/main/recipe/activate-clang.sh
+# to use host python; requires that [binaries] section is last in meson_cross_file
+echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+
+if [[ $target_platform == "osx-arm64" ]]; then
+    # currently cannot properly detect long double format
+    # on osx-arm64 when cross-compiling in QEMU, see
+    # https://github.com/numpy/numpy/pull/24414
+    sed -i.bak "s@\[properties\]@[properties]\nlongdouble_format = 'IEEE_DOUBLE_LE'@g" ${CONDA_PREFIX}/meson_cross_file.txt
+fi
 
 # meson-python already sets up a -Dbuildtype=release argument to meson, so
 # we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
@@ -14,4 +29,4 @@ $PYTHON -m build -w -n -x \
     -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
 
-$PYTHON -m pip install --find-links dist f90wrap
+$PYTHON -m pip install dist/f90wrap*.whl
